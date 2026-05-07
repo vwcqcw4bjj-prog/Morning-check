7ac3b7d5bbc8650cce47fab0b9dad8370d804c24
vwcqcw4bjj-prog <vwcqcw4bjj@privaterelay.appleid.com>
2026-05-08 06:14:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -351,4 +351,5 @@ def _build_page(df_week, df_market, updated_at, running):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
